0491b7e4fbd7ae802da4c67990e1ebbbe117cbae
SageM <madduxsage@gmail.com>
2023-11-29 14:37:03 -0500

    working axios call in home displaying photos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-export default function Footer() {
 
 
-
-
-    return(
-        <div>
-            <h1>this is Footer </h1>
-            
-        </div>
-    )
-}
+function Footer() {
+    return (
+      <>
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Nav className="me-auto">
+              <Nav.Link href="linkedin">Linkedin</Nav.Link>
+              <Nav.Link href="home">Home</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
+    );
+  }
+  
+  export default Footer;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,44 @@
-import Nav from "./Nav"
-import Footer from "./Footer"
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import Nav from "./Nav";
+import Footer from "./Footer";
 
 export default function Home() {
+    const [photos, setPhotos] = useState([]);
 
+    useEffect(() => {
+        const getPhotos = async () => {
+            try {
+                const response = await axios.get(`http://127.0.0.1:8000/photos/`);
+                console.log(response.data);
+                setPhotos(response.data);
+            } catch (error) {
+                console.error("Error fetching photos:", error);
+            }
+        };
 
+        getPhotos();
+    }, []);
 
-
-    return(
+    return (
         <div className="Homediv">
             <div className="navhomediv">
-                <Nav/>
+                <Nav />
             </div>
             <div className="homecontentdiv">
-                <h1>this is home </h1>
-                
+                <h1>This is home</h1>
+                {photos.map((photo) => (
+                    <div key={photo.id} className="photo-container">
+                        <img src={photo.image} alt={photo.photo_name} />
+                        <p>{photo.photo_name}</p>
+                        <p>{photo.user.email}</p>
+                        {/* Add more details or styling as needed */}
+                    </div>
+                ))}
             </div>
             <div className="footerhomediv">
-                <Footer/>
+                <Footer />
             </div>
         </div>
-    )
+    );
 }
