735baff27c15a19e6a9df92ea61105e391d2946d
SageM <madduxsage@gmail.com>
2023-12-04 16:00:52 -0500

    added a bunch of photo functionality, uploadname, photo, userId

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -7,85 +7,108 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
-
 export default function Photos() {
     const [photos, setPhotos] = useState([]);
-    const [Comments, setComments] = useState({});
+    const [comments, setComments] = useState({});
+    const [newPhoto, setNewPhoto] = useState(null);
+    const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [photoName, setPhotoName] = useState("");
 
     useEffect(() => {
-        const getPhotos = async () => {
+        const fetchData = async () => {
             try {
-                const response = await axios.get(`http://127.0.0.1:8000/photos/`);
-                console.log(response.data);
-                setPhotos(response.data);
+                const responsePhotos = await axios.get(`http://127.0.0.1:8000/photos/`);
+                const responseUsers = await axios.get(`http://127.0.0.1:8000/users/`);
+
+                setPhotos(responsePhotos.data);
+                setUsers(responseUsers.data);
             } catch (error) {
-                console.error("Error fetching photos:", error);
+                console.error("Error fetching data:", error);
             }
         };
 
-        getPhotos();
+        fetchData();
     }, []);
 
+    const handlePhotoChange = (event) => {
+        const file = event.target.files[0];
+        setNewPhoto(file);
+    };
 
-    useEffect(() => {
-        const getComments = async () => {
-            try {
-                const response2 = await axios.get(`http://127.0.0.1:8000/comments/`);
-                console.log(response2.data);
-                setComments(response2.data);
-            } catch (error) {
-                console.error("Error fetching Comments:", error);
-            }
-        };
+    const handleUserChange = (event) => {
+        const userId = event.target.value;
+        setSelectedUser(userId);
+    };
 
-        getComments();
-    }, []);
-
-
-
-
-
+    const handleUpload = async () => {
+        try {
+            const formData = new FormData();
+            formData.append('image', newPhoto);
+            formData.append('user', selectedUser);
+            formData.append('photo_name', photoName); // Add this line
+    
+            // Post request
+            await axios.post(`http://127.0.0.1:8000/photos/upload/`, formData);
+    
+            // Refresh the photos after upload
+            const photoResponse = await axios.get(`http://127.0.0.1:8000/photos/`);
+            setPhotos(photoResponse.data);
+        } catch (error) {
+            console.error("Error uploading photo:", error);
+        }
+    };
+    
     
 
-return (
-    <div className="photosmaindiv">
-        <Nav />
-        <h3>This is photos</h3>
-        <Container>
-            {photos.map((photo, index) => (
-                index % 2 === 0 && (
-                    <Row key={photo.id}>
-                        <Col>
-                            <Card>
-                                <Card.Img src={photo.image} alt={photo.photo_name} className="img-fluid" />
-                                <Card.Body>
-                                    <h5>{photo.photo_name}</h5>
-                                    {/* Add other details you want to display */}
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        {index + 1 < photos.length && (
+    return (
+        <div className="photosmaindiv">
+            <Nav />
+            <h3>View your photos here</h3>
+            <Container>
+                <input type="file" accept="image/*" onChange={handlePhotoChange} />
+                <input type="text" placeholder="Enter photo name" value={photoName} onChange={(e)=> setPhotoName(e.target.value)}/>
+                
+                {/* Dropdown to select a user */}
+                <select onChange={handleUserChange}>
+                    <option value="">Select a user</option>
+                    {users.map((user) => (
+                        <option key={user.id} value={user.id}>
+                            {user.name}
+                        </option>
+                    ))}
+                </select>
+
+                <button onClick={handleUpload}>Upload Photo</button>
+
+                {photos.map((photo, index) => (
+                    index % 2 === 0 && (
+                        <Row key={photo.id}>
                             <Col>
                                 <Card>
-                                    <Card.Img src={photos[index + 1].image} alt={photos[index + 1].photo_name} className="img-fluid" />
+                                    <Card.Img src={photo.image} alt={photo.photo_name} className="img-fluid" />
                                     <Card.Body>
-                                        <h5>{photos[index + 1].photo_name}</h5>
+                                        <h5>{photo.photo_name}</h5>
                                         {/* Add other details you want to display */}
                                     </Card.Body>
                                 </Card>
                             </Col>
-                        )}
-                    </Row>
-                )
-            ))}
-        </Container>
-        <Footer />
-    </div>
-);
-
-    
+                            {index + 1 < photos.length && (
+                                <Col>
+                                    <Card>
+                                        <Card.Img src={photos[index + 1].image} alt={photos[index + 1].photo_name} className="img-fluid" />
+                                        <Card.Body>
+                                            <h5>{photos[index + 1].photo_name}</h5>
+                                            {/* Add other details you want to display */}
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            )}
+                        </Row>
+                    )
+                ))}
+            </Container>
+            <Footer />
+        </div>
+    );
 }
-
-
-
-
